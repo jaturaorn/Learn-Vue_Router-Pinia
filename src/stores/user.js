f51a268a36c8402518df62e5cb744d41d5a4b569
jaturaporn <jaturapornff@gmail.com>
2023-12-05 10:42:08 +0700

    finish to call pinia

--- src/stores/user.js
+++ src/stores/user.js
@@ -4,9 +4,21 @@ import { defineStore } from "pinia";
 export const useUserStore = defineStore('user', {
     // ใส่วงเล็บเพราะไม่ต้องพิมพ์ Return
     state: () => ({
-        
+        firstname: 'test',
+        lastname: 'oo'
     }),
     actions: {
-        
+        changeName (formData) {
+            /*
+                หน้าตาของผลลัพธ์จะเป็นแบบนี้
+
+                {
+                    firstname: '',
+                    lastname: ''
+                }
+            */
+            this.firstname = formData.firstname
+            this.lastname = formData.lastname
+        }
     }
 })
--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,20 +1,54 @@
 <script setup>
-import { onMounted } from "vue";
+import { onMounted, ref } from "vue";
 import { RouterLink, useRoute, useRouter } from "vue-router";
+import { useUserStore } from "../stores/user";
 
 const router = useRouter()
+
+const userStore = useUserStore()
+
+const isUpdated = ref(false)
+const firstname = ref('')
+const lastname = ref('')
 
 const backToHome = () => {
     // ทำอะไรสักอย่างก่อน
     router.push({ name: 'home' })
 }
+
+const updateProfile = () => {
+    const formData = {
+        firstname: firstname.value,
+        lastname: lastname.value
+    }
+    userStore.changeName(formData)
+    // console.log(firstname.value);
+    // console.log(lastname.value);
+}
+
+onMounted(() => {
+    console.log(userStore.firstname);
+    console.log(userStore.lastname);
+})
 </script>
 
 <template>
     <div>
+        Profile from stores
+        <div>Firstname {{ userStore.firstname }}</div>
+        <div>Lastname {{ userStore.lastname }} </div>
+    </div>
+    <div>
         Profile Vue
-        <div>Firstname <input type="text"></div>
-        <div>Lastname <input type="text"></div>
+        <div>
+            Firstname <input type="text" v-model="firstname">
+        </div>
+        <div>
+            Lastname <input type="text" v-model="lastname">
+        </div>
+        <div>
+            <button @click="updateProfile()"> Update profile</button>
+        </div>
         <div>
                 <button @click="backToHome()">Back to home</button>
         </div>
