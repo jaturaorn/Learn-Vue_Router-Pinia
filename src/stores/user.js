c5e81b28bae3b4588f97d548955230c23dc3b912
jaturaporn <jaturapornff@gmail.com>
2023-12-05 11:26:21 +0700

    start in workshop with pinia state management

--- src/stores/user.js
+++ src/stores/user.js
@@ -1,30 +1,46 @@
 import { ref, computed } from "vue";
 import { defineStore } from "pinia";
 
-export const useUserStore = defineStore('user', {
-    // ใส่วงเล็บเพราะไม่ต้องพิมพ์ Return
+export const useBookStore = defineStore('book', {
     state: () => ({
-        firstname: 'test',
-        lastname: 'oo'
+        book: []
     }),
-    getters: {
-        fullname (state) {
-            return `${state.firstname} ${state.lastname}`
-        }
-    },
     actions: {
-        changeName (formData) {
-            /*
-                หน้าตาของผลลัพธ์จะเป็นแบบนี้
-
-                {
-                    firstname: '',
-                    lastname: ''
-                }
-
-            */
-            this.firstname = formData.firstname
-            this.lastname = formData.lastname
+        addBook (bookData) {
+            this.books.push(bookData)
+        },
+        removeBook (bookIndex) {
+            this.book.splice(bookIndex , 1)
+        },
+        editBook (bookIndex, bookData) {
+            // splice ใช้กับ case update ได้เหมือนกัน
+            this.book.splice(bookIndex , 1, bookData)
         }
     }
+    
+    // // ใส่วงเล็บเพราะไม่ต้องพิมพ์ Return
+    // state: () => ({
+    //     firstname: 'test',
+    //     lastname: 'oo'
+    // }),
+    // getters: {
+    //     fullname (state) {
+    //         return `${state.firstname} ${state.lastname}`
+    //     }
+    // },
+    // actions: {
+    //     changeName (formData) {
+    //         /*
+    //             หน้าตาของผลลัพธ์จะเป็นแบบนี้
+
+    //             {
+    //                 firstname: '',
+    //                 lastname: ''
+    //             }
+
+    //         */
+    //         this.firstname = formData.firstname
+    //         this.lastname = formData.lastname
+    //     }
+    // }
 })
